e2094f1b8eff5dae65c669c591300eaa8c639aa6
dben <sam.dben@gmail.com>
2024-05-21 15:24:35 -0700

    hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -57,32 +57,34 @@ class Server:
         except IndexError:
             return []
 
-     def get_hyper(self, page: int =1, page_size: int = 10) -> dict:
-         """return the appropiate page of the dataset"""
-         assert type(page) is int and page > 0
-         assert type(page_size) is int and page_size > 0
 
-         data = self.get_page(page, page_size)
-         total_pages = math.ceil(len(self.dataset()) / page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """returns a dictionary containing the following key-value pairs
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-         start, end = index_range(page, page_size)
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
-         # estimating the next page
-         if (page < total_pages):
-             next_page = page+1
-         else:
-             next_page = None
+        start, end = index_range(page, page_size)
 
-          # estimating the previous page
-          if (page == 1):
-              prev_page = None
-          else:
-              prev_page = page - 1
+        # estimating the next page
+        if (page < total_pages):
+            next_page = page+1
+        else:
+            next_page = None
 
-          return {'page_size': len(data),
-                  'page': page,
-                  'data': data,
-                  'next_page': next_page,
-                  'prev_page': prev_page,
-                  'total_pages': total_pages
-                  }
+        # estimating the previous page
+        if (page == 1):
+            prev_page = None
+        else:
+            prev_page = page - 1
+
+        return {'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
