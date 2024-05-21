6ae020c131709b18e778fa95176f2eca22dcc9a7
dben <sam.dben@gmail.com>
2024-05-21 15:27:25 -0700

    hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -59,8 +59,7 @@ class Server:
 
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """returns a dictionary containing the following key-value pairs
-        """
+        """returns a dictionary containing the following key-value pairs """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
 
