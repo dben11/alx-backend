8fe0798799ad532d5f08d41ed878caa9cb6abe75
dben <sam.dben@gmail.com>
2024-05-22 19:31:30 -0700

    basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 """Create a class BasicCache that inherits from
-BaseCaching and is a caching system
+BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+def get(self, key):
+Must return the value in self.cache_data linked
+to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
 """
 
 
@@ -8,24 +22,32 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ You must use self.cache_data - dictionary from the
-    parent class BaseCaching
-This caching system doesn’t have limit
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the item value for the key key.
-If key or item is None, this method should not do anything.
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in self.cache_data, return None.
+    """_summary_
     """
 
     def __init__(self):
+        """_summary_
+        """
         super().__init__()
+
     def put(self, key, item):
-        if key is None and item is None:
+        """_summary_
+
+        Args:
+                key (_type_): _description_
+                item (_type_): _description_
+        """
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """return the value in self.cache_data linked to key
+
+        Args:
+                key (_type_): _description_
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
