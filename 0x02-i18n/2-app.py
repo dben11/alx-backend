8828c278c96bf6502a90d22359fd09cf440f264d
dben <sam.dben@gmail.com>
2024-05-29 11:57:34 -0700

    get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A function decorator
+"""A simple flask app
 """
 
 
@@ -8,34 +8,38 @@ from flask_babel import Babel
 
 
 class Config(object):
-    """_sumation_
+    """_summary_
 
-    Return:
-    	   _type_: _description_
+    Returns:
+            _type_: _description_
     """
-    LANGUAGE = ['en', 'fr']
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-    # configure the flask app
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    babel = Babel(app)
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
-    @babel.localeselector
-    def get_locale():
-	"""_sum_
+@babel.localeselector
+def get_locale():
+    """_summary_
 
-	Returns:
-		_type_: _description_
-	"""
-	return request.accept_language.best_match(app.config['LANGUAGES'])
+    Returns:
+            _type_: _description_
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-    @app.route('/')
-    def index():
-	"""_sum_
-	"""
-	return render_template('2-index.html')
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
