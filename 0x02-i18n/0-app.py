ab0c3a1f944fadaab0ec12a4d5eef2e3a1c0a533
dben <sam.dben@gmail.com>
2024-05-28 19:07:39 -0700

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""First you will setup a basic Flask app in 0-app.py. Create a single /
- route and an index.html
-template that simply outputs “Welcome to Holberton” as page title (<title>) and
+"""First you will setup a basic Flask app in 0-app.py. Create a
+single /
+route and an index.html
+template that simply outputs “Welcome to Holberton” as page title
+(<title>) and
 “Hello world” as header (<h1>
 """
 
@@ -10,6 +12,7 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
