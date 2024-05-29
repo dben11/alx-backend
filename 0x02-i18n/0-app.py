86603c3c301311fa26cc5ded4496615dc0aab23b
dben <sam.dben@gmail.com>
2024-05-28 18:56:52 -0700

    basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,10 @@ template that simply outputs “Welcome to Holberton” as page title (<title>) 
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '/'
+    return 'Welcome to Holberton'
